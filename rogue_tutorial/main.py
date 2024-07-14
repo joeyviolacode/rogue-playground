8e77f5e4ab790c03f2ebb65b7139c0310669f15b
joeyviolacode <joeyviolacode@gmail.com>
2024-07-14 17:13:26 -0400

    Gitignore fun

--- rogue_tutorial/main.py
+++ rogue_tutorial/main.py
@@ -3,6 +3,8 @@ import tcod
 def main():
     screen_height = 50
     screen_width = 80
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -17,14 +19,13 @@ def main():
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(x=1, y=1, string="@")
+            root_console.print(x=player_x, y=player_y, string="@")
 
             context.present(root_console)
 
             for event in tcod.event.wait():
                 if event.type == "QUIT":
                     raise SystemExit()
-
 
 if __name__ == "__main__":
     main()
