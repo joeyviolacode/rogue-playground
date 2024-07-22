d446b73b39e24edee469ba6520b5c3f940947282
joeyviolacode <joeyviolacode@gmail.com>
2024-07-21 22:25:00 -0400

    Finished

--- rogue_tutorial/engine.py
+++ rogue_tutorial/engine.py
@@ -7,6 +7,7 @@ from actions import Action, MovementAction, EscapeAction
 from entity import Entity
 from game_map import GameMap
 from input_handlers import EventHandler
+from tcod.map import compute_fov
 
 class Engine:
     def __init__(self, entities: Set[Entity], event_handler: EventHandler, game_map: GameMap, player: Entity):
@@ -14,6 +15,7 @@ class Engine:
         self.event_handler = event_handler
         self.game_map = game_map
         self.player = player
+        self.update_fov()
 
     def handle_events(self, events: Iterable[Any]) -> None:
         for event in events:
@@ -24,11 +26,22 @@ class Engine:
 
             action.perform(self, self.player)
 
+            self.update_fov()
+
+    def update_fov(self) -> None:
+        self.game_map.visible[:] = compute_fov(
+            self.game_map.tiles["transparent"],
+            (self.player.x, self.player.y),
+            radius=8,
+        )
+        self.game_map.explored |= self.game_map.visible
+
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
 
         for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, fg=entity.color)
+            if self.game_map.visible[entity.x, entity.y]:
+                console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
         context.present(console)
 
--- rogue_tutorial/game_map.py
+++ rogue_tutorial/game_map.py
@@ -1,16 +1,32 @@
+from __future__ import annotations
+
+from typing import Iterable, TYPE_CHECKING
+
 import numpy as np
 from tcod.console import Console
 
 import tile_types
 
+if TYPE_CHECKING:
+    from entity import Entity
+
 class GameMap:
-    def __init__(self, width: int, height: int):
+    def __init__(self, width: int, height: int, entities: Iterable[Entity] = ()):
         self.width = width
         self.height = height
+        self.entities = entities
+
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
+
+        self.visible = np.full((width, height), fill_value=False, order="F")
+        self.explored = np.full((width, height), fill_value=False, order="F")
 
     def in_bounds(self, x: int, y: int) -> bool:
         return 0 <= x <= self.width and 0 <= y <= self.height
     
     def render(self, console: Console) -> None:
-        console.tiles_rgb[0: self.width, 0: self.height] = self.tiles["dark"]
+        console.tiles_rgb[0: self.width, 0: self.height] = np.select(
+            condlist=[self.visible, self.explored],
+            choicelist=[self.tiles["light"], self.tiles["dark"]],
+            default=tile_types.SHROUD,
+        )
